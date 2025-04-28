924556c0724348e5ac03e8e201f0a3d5d38db3cd
Chunamporn <chunna@bu.edu>
2025-04-27 23:21:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ with tab1:
     ROE = PMR × ATR × EQM
     This breakdown helps understand the underlying drivers of company profitability.
     """)
-    st.image("DuPont2.png", caption="Dupont Analysis Model")
+    st.image("DuPont 2.png", caption="Dupont Analysis Model")
 with st.expander("What is Dupont Analysis and why is projected ROE important?"):
     st.markdown("""
     **About Dupont Analysis**  
